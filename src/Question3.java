c480be2564766e067c08a02a21b57139c3731eeb
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-10-17 10:07:07 -0400

    Finished Question3

---
+++ src/Question3.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl3669
+ */
+public class Question3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create an input for the user 
+        Scanner input = new Scanner(System.in);
+        String num;
+        
+        // ask user for their numbers, and store it
+        System.out.println("Please enter 4 numbers on seperate lines.");
+        double first = input.nextDouble();
+        double second = input.nextDouble();
+        double third = input.nextDouble();
+        double fourth = input.nextDouble();
+        
+        // output the numbers in order the user had inputted them
+        System.out.println("Your numbers were " + first + ", " + second + ", " + third + ", " + fourth + ".");
+        
+        
+    }
+}
