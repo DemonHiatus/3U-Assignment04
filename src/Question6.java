9216fa7da447dc6d2f5bab6f6e18fe8a90b5ca81
baayl <baayl@192.168.0.13>
2016-10-17 18:17:09 -0400

    Having some troubles rounding off.

---
+++ src/Question6.java
@@ -0,0 +1,66 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl
+ */
+public class Question6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Number of daytime minutes?");
+        double day = input.nextDouble();
+        
+        System.out.println("Number of evening minutes?");
+        double eve = input.nextDouble();
+        
+        System.out.println("Number of weekend minutes?");
+        double weekend = input.nextDouble();
+        
+        // storing Plan A 
+        double planA = (day - 100) * 0.25;
+        if (planA < 0){
+            planA = 0;
+        }
+        double alpha = planA + (eve * 0.15) + (weekend * 0.20);
+        
+        // storing Plan B
+        double planB = (day - 250) * 0.45;
+        if (planB < 0){
+            planB = 0;
+        }
+        double beta = planB + (eve * 0.35) + (weekend * 0.25);
+        
+        alpha = Math.round(alpha * 100) / 100;
+        beta = Math.round(beta * 100) / 100;
+        
+        
+        // output of which one is cheaper
+        System.out.println("Plan A cost $" + alpha + ".");
+        System.out.println("Plan B cost $" + beta + ".");
+        
+        if (alpha == beta) {
+            System.out.println("Plan A and B are the same price.");
+        } else if (alpha > beta){
+            System.out.println("Plan B is the cheapest.");
+        } else if (beta > alpha){
+            System.out.println("Plan A is the cheapest.");
+        }
+            
+                
+            
+        
+    }
+    
+}
