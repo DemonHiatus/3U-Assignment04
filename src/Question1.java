f09b2f3d70b1fa068414b4f9a68736ab8d0fffbb
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-10-17 10:06:55 -0400

    Finished Question1

---
+++ src/Question1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl3669
+ */
+public class Question1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // create the input for the user 
+        Scanner input = new Scanner(System.in);
+        
+        // ask the user for their name
+        System.out.println("please enter your name");
+        // get the name from the user 
+        String name = input.nextLine();
+        // say hello
+        System.out.println("Hello " + name + ". How are you today?");
+    }
+}
