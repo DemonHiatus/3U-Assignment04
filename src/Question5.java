e99f9c1226193f2eb5261029f37307466c002b40
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-10-17 10:10:53 -0400

    Just did comments and ask users name.

---
+++ src/Question5.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl3669
+ */
+public class Question5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         // create the input for the user 
+        Scanner input = new Scanner(System.in);
+        String name;
+        
+        // ask for users name
+        System.out.println("Please enter your name");
+        name = input.nextLine();
+        
+        // Test #1 Average
+        // Test #2 Average
+        // Test #3 Average 
+        // Test #4 Average 
+        // Test #5 Average
+    }
+}
