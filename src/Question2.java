61cd4e0a785a20ad36c4b69963b64789487ad310
baayl3669 <baayl3669@HRH-IC0027339.wrdsb.ca>
2016-10-17 10:07:02 -0400

    Finished Question2

---
+++ src/Question2.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author baayl3669
+ */
+public class Question2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create an input for the user 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the measurement in inches you want to convert");
+        
+        // inches to cm
+        double inch = input.nextDouble();
+        double cm = (double) (inch * 2.54);
+        
+        // output the converstion
+        System.out.println(inch + " inches is the same measurement as "  + cm +  " cm.");
+        System.out.println("You did it!");
+            
+        }
+    
+            
+            
+        }
+                
+                
+
